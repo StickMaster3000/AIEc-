888f94a795d3d8e80a7b655f9432404288ff4927
StickMaster3000 <jalencavanagh@gmail.com>
2024-03-27 14:24:57 +1100

    added A1

---
+++ ICTPRG302 Intro to Programming/ICTPRG302 Intro to Programming/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace ICTPRG302_Intro_to_Programming
+{
+	class Program
+	{
+		/// <summary>
+		/// This is the starting location for the program.
+		/// </summary>
+		/// <param name="args">
+		/// A list of strings passed in to the program
+		/// from the command-line when it was started
+		/// </param>
+		static void Main(string[] args)
+		{
+			// TODO: Add your main code here 
+		}
+	}
+}
